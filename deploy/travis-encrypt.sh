537b8b0e6cc5c3558d4b9dc7ea805ec73240efbd
avingtier <avingtier@gmail.com>
2020-12-12 16:44:28 +0100

    remove old unused files, fix docker config

--- deploy/travis-encrypt.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt-get update
-sudo apt-get install -y ruby-full
-sudo gem install travis
-travis login --org
-touch .travis.yml && travis encrypt-file $1 --add
--- docker/stop.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo -e "\e[32m> Downing containers ...\e[0m"
-sudo docker-compose down --remove-orphans
